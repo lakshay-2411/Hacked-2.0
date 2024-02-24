a8b9d9a0979d441b76b1eb40a5bd8dbe83bf11e8
Lakshay <121676336+lakshay-2411@users.noreply.github.com>
2024-02-24 20:54:38 +0530

    Update sponsors.jsx

--- src/components/hero/sponsors.jsx
+++ src/components/hero/sponsors.jsx
@@ -51,7 +51,7 @@ const sponsors = () => {
         </div>
         <Faqs />
         {<Contact /> }
-        {/* {<Convener />}
+        {<Convener />}
         {<Coordinators />} */}
       </div>
     </React.Fragment>
